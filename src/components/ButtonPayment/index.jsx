1c87ff12b812b89f02f81deefcfa704ae366aab5
Defriyhdr <Defiyhdr@gmail.com>
2023-12-08 15:25:06 +0700

    add component btn payment

---
+++ src/components/ButtonPayment/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const ButtonPayment = () => {
+  return (
+    <div>
+      <button className="btn-payment">Bayar</button>
+    </div>
+  );
+};
+
+export default ButtonPayment;
